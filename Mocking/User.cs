84bea6875873e83201cb1069c23547b9bd70b735
kasp3939 <kasp3939@edu.eal.dk>
2016-10-11 09:06:40 +0200

    1

--- Mocking/User.cs
+++ Mocking/User.cs
@@ -2,14 +2,38 @@
 
 namespace Mocking
 {
-    public class User
+    public class User : ILoginModule
     {
         public int NumMessagesCreated { get; internal set; }
         public string Password { get; internal set; }
         public object Rights { get; internal set; }
         public string UserName { get; internal set; }
 
-        internal string ViewAllEmployees()
+        public string ViewAllEmployees()
+        {
+            return "Here is the list";
+        }
+        public void Login(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResendPassword(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void EditEmployeeName(string name, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void EditEmployeeWage(int wage, int v)
         {
             throw new NotImplementedException();
         }
